e3b9c814c95b33853e263e0c8e9220f147284e5c
j.faassen <j.faassen@linkorb.com>
2013-09-07 20:53:20 +0200

    Add image url

--- src/Tangler/Module/Imap/Module.php
+++ src/Tangler/Module/Imap/Module.php
@@ -12,6 +12,7 @@ class Module extends AbstractModule implements ModuleInterface
         $this->setKey('imap');
         $this->setLabel('IMAP module');
         $this->setDescription('This is the IMAP module');
+        $this->setImageUrl('http://carolwatkins.co.uk/wp-content/uploads/2012/06/incoming-email-icon-150x150.jpg');
 
         $this->setTriggers(array(
             new \Tangler\Module\Imap\NewEmailTrigger()
